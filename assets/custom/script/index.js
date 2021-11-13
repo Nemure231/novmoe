aecc77863703cd8fdeaab6a9237a25f06dd89fb9
Nemure231 <neruhi5321@gmail.com>
2021-11-13 23:47:54 +0700

    Menata card cerita

--- assets/custom/script/index.js
+++ assets/custom/script/index.js
@@ -53,3 +53,65 @@ document.addEventListener('alpine:init', () => {
         }
     }));
 });
+
+
+function ready(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(function () {
+
+   
+        var tombolBlurb = document.getElementsByClassName('tombol-blurb-pendek');
+        Array.prototype.forEach.call(tombolBlurb, function (element) {
+            element.addEventListener('click', function () {
+
+               var blurbPendek = element.previousElementSibling;
+               var blurbPendekHidden = blurbPendek.previousElementSibling;
+
+               if (blurbPendekHidden.classList.contains('hidden')) {
+                   blurbPendek.classList.add('hidden');
+                   blurbPendekHidden.classList.remove('hidden');
+                   element.innerText = 'Show less';
+               }else{
+                blurbPendek.classList.remove('hidden');
+                blurbPendekHidden.classList.add('hidden');
+                element.innerText = 'Show more';
+               }
+                
+    
+            });
+        });
+    
+
+
+    var blurbPendek = document.getElementsByClassName('blurb-pendek');
+    var num = 7;
+
+    for (var i = 0; i < blurbPendek.length; i++) {
+
+    
+
+        if (blurbPendek[i].innerText.split(" ").length > num) {
+            var mk = blurbPendek[i].innerText.split(" ").splice(0, num).join(" ") + ' ....';
+            blurbPendek[i].innerText = mk;
+
+        } else {
+            blurbPendek[i].innerText = blurbPendek[i].innerText;
+            blurbPendek[i].nextElementSibling.remove();
+
+        }
+
+    }
+
+
+
+
+
+
+
+});
