f472525c292f3d592d594af9cff23f2319f96dbf
Nemure231 <neruhi5321@gmail.com>
2021-11-18 13:48:38 +0700

    Membetulkan ukuran margin carousel

--- assets/custom/script/index.js
+++ assets/custom/script/index.js
@@ -119,7 +119,7 @@ ready(function () {
 
 var swiper_mobile = new Swiper(".myswiper-mobile", {
     slidesPerView: 3,
-    spaceBetween: 165,
+    spaceBetween: 160,
     loop: true,
     freeMode: true,
     // navigation: {
