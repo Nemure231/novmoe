6381ab75c1fab4f1a0c2a6504bbe8ce9519c2095
Nemure231 <neruhi5321@gmail.com>
2021-12-06 12:33:57 +0700

    Mencoba membuat carousel sendiri

--- assets/custom/script/index.js
+++ assets/custom/script/index.js
@@ -114,6 +114,34 @@ function ready(fn) {
 
 ready(function () {
 
+    const slider = document.querySelector('.grab-scroll');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    
+    slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 3; //scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+      console.log(walk);
+    });
+
     var swiper_mobile= new Swiper(".myswiper-mobile", {
         slidesPerView: 3,
         spaceBetween: 147,
