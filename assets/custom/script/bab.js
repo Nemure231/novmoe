92215025b8341c1740114828a94658d8f66335fe
Nemure231 <neruhi5321@gmail.com>
2021-12-15 15:48:43 +0700

    Menambahkan komentar pada file cerita dan menambahkan halaman bab

--- assets/custom/script/bab.js
+++ assets/custom/script/bab.js
@@ -57,45 +57,7 @@ document.addEventListener('alpine:init', () => {
     }));
 });
 
-document.addEventListener('alpine:init', () => {
-    Alpine.data('usulan', () => ({
-        komentar: {
-            1: {
-                id: 1,
-                nama: 'Angelica',
-                username: '@Ange',
-                foto: './assets/img/profile.png',
-                komentar: 'Ini ada lanjutannya lagi gak gan?',
-                tanggal: 'hari ini',
-                reaksi: 'fas fa-thumbs-up text-green-400'
-            },
-            2: {
-                id: 2,
-                nama: 'A02',
-                username: '@Nine',
-                foto: './assets/img/profile_2.png',
-                komentar: 'Menurutku ending di volume 1 ini agak lemah, banyak masalah yang belum kelar, yang menurutku bisa diberesin di volume ini sih.',
-                tanggal: 'kemarin',
-                reaksi: 'fas fa-thumbs-down text-red-400'
-            },
 
-        },
-
-
-    }));
-});
-
-
-
-function potong_komentar(komen) {
-    if (komen.split(" ").length > 50) {
-        var mk = komen.split(" ").splice(0, 50).join(" ") + ' ....';
-        return komen = mk;
-    } else {
-        return komen = komen;
-
-    }
-}
 
 function ready(fn) {
     if (document.readyState != 'loading') {
@@ -110,33 +72,7 @@ function ready(fn) {
 
 ready(function () {
 
-    // const slider = document.querySelector('.grab-scroll');
-    // let isDown = false;
-    // let startX;
-    // let scrollLeft;
-    
-    // slider.addEventListener('mousedown', (e) => {
-    //   isDown = true;
-    //   slider.classList.add('active');
-    //   startX = e.pageX - slider.offsetLeft;
-    //   scrollLeft = slider.scrollLeft;
-    // });
-    // slider.addEventListener('mouseleave', () => {
-    //   isDown = false;
-    //   slider.classList.remove('active');
-    // });
-    // slider.addEventListener('mouseup', () => {
-    //   isDown = false;
-    //   slider.classList.remove('active');
-    // });
-    // slider.addEventListener('mousemove', (e) => {
-    //   if(!isDown) return;
-    //   e.preventDefault();
-    //   const x = e.pageX - slider.offsetLeft;
-    //   const walk = (x - startX) * 3; //scroll-fast
-    //   slider.scrollLeft = scrollLeft - walk;
-    //   console.log(walk);
-    // });
+ 
 
 
  
